2a373a5534099ac1bb227667998426136add50b9
Rodney Gallardo <rodneygallardo98@gmail.com>
2024-11-25 11:52:23 -0400

    "AvancePagina"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+ import React from "react";
 import Dash from "../components/layouts/Dash";
-
+import '../assets/css/Home.css';
 function Home(){
     return (
         <>
             <Dash></Dash>
             <div>
+              
               <div>
               <Carrusel></Carrusel>
               </div>
+              
               <div>
                 <h1>Descripcion de la empresa</h1>
               </div>
@@ -20,7 +22,35 @@ function Home(){
 
 function Carrusel(){
     return (
-    <div> <h1>Carrusel</h1></div>
+      <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" >
+              
+        <div className="carousel-inner">
+                
+          <div className="carousel-item active">
+            <img src="src\assets\img\imagen4g.jpg" className="d-block w-100" alt="..."/>
+          </div>
+
+          <div className="carousel-item">
+            <img src="src\assets\img\imagen4g.jpg" className="d-block w-100" alt="..."/>
+          </div>
+
+          <div className="carousel-item">
+            <img src="src\assets\img\imagen4g.jpg" className="d-block w-100" alt="..."/>
+          </div>
+
+        </div>
+        
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+
+      </div>
     )
 }
 export default Home;
