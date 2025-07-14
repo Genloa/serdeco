896219881e14a7d9a84fb9d382a2012ee828843c
Genesis L. Delgado H. <98361965+Genloa@users.noreply.github.com>
2025-07-14 16:39:26 -0400

    carrusel, somos y testimonios

--- src/components/layouts/Dash.jsx
+++ src/components/layouts/Dash.jsx
@@ -1,63 +1,116 @@
 import React from "react";
+import logo from "../../assets/img/logo_1.png";
+import logo2 from "../../assets/img/logo_2.png";
 
 function Dash() {
-    return (
-        <>
-           <Navbar></Navbar> 
-           
-        </>
-
-    )
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
 }
 
-function Navbar (){
-    return (
-        <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-white ">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                    <img src="src\assets\img\logo.png" alt="" width="150" height="30"/>
-                    </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
-                        <ul className="navbar-nav ">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Quienes Somos
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Historia</a></li>
-                                    <li><a className="dropdown-item" href="#">Misión Y Visión</a></li>
-                                    <li><a className="dropdown-item" href="#">Valores</a></li>
-                                </ul>
-                               
-                            </li>
-                            <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Atención al Cliente
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Preguntas Frecuentes</a></li>
-                                    <li><a className="dropdown-item" href="#">Oficinas de Atención</a></li>
-                                    <li><a className="dropdown-item" href="#">Reclamo</a></li>
-                                </ul>
-                               
-                            </li>
-                            <li className="nav-item ">
-                            <a className="nav-link" href="#">Mapa de Cobertura</a>
-                            </li>
-                            <li className="nav-item ">
-                            <a className="nav-link" href="#">Noticias</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <button  type="button" class="btn text-light fw-bold" style={{background: '#8FC642'}} >Contactanos</button>
-                </div>
-        </nav>
-    )
+function Navbar() {
+  return (
+    <nav
+      className="navbar navbar-expand-lg fixed-top navbar-light "
+      style={{ backgroundColor: "#1c5e32" }}
+    >
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img src={logo} alt="" width="140%" height="35" />
+        </a>
+        <a className="navbar-brand ms-3 mb-1 " href="#">
+          <img src={logo2} alt="" width="90%" height="25" />
+        </a>
+
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarNavDropdown"
+        >
+          <ul className="navbar-nav ">
+            <li className="nav-item">
+              <a
+                className="nav-link active text-light "
+                aria-current="page"
+                href="#"
+              >
+                Inicio
+              </a>
+            </li>
+            <li className="nav-item dropdown ">
+              <a
+                className="nav-link dropdown-toggle text-light "
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Quienes Somos
+              </a>
+              <ul className="dropdown-menu  bg-success">
+                <li>
+                  <a className="dropdown-item text-light  " href="#">
+                    Historia
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item text-light" href="#">
+                    Misión Y Visión
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item text-light " href="#">
+                    Valores
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle text-light "
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Atención al Cliente
+              </a>
+              <ul className="dropdown-menu bg-success">
+                <li>
+                  <a className="dropdown-item text-light " href="#">
+                    Preguntas Frecuentes
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item text-light " href="#">
+                    Oficinas de Atención
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item text-light " href="#">
+                    Reclamo
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item ">
+              <a className="nav-link text-light" href="#">
+                Mapa de Cobertura
+              </a>
+            </li>
+            <li className="nav-item ">
+              <a className="nav-link text-light" href="#">
+                Noticias
+              </a>
+            </li>
+          </ul>
+        </div>
+        {/* <button type="button" className="btn text-light fw-bold bg-warning">
+          Contactanos
+        </button> */}
+      </div>
+    </nav>
+  );
 }
-export default Dash
+export default Dash;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,77 +1,148 @@
- import React from "react";
+import React from "react";
 import Dash from "../components/layouts/Dash";
-import '../assets/css/Home.css';
-function Home(){
-    return (
-        <>
-            <Dash></Dash>
-            <div>
-              
-              <div>
-              <Carrusel></Carrusel>
-              </div>
-              
-              <div>
-                <Servicios></Servicios>
-              </div>
-              
-            </div>
-        </>
-    );
-}
-
-function Carrusel(){
-    return (
-      <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" >
-              
-        <div className="carousel-inner">
-                
-          <div className="carousel-item active">
-            <img src="src\assets\img\imagen4g.jpg" className="d-block w-100" alt="..."/>
-          </div>
-
-          <div className="carousel-item">
-            <img src="src\assets\img\imagen4g.jpg" className="d-block w-100" alt="..."/>
-          </div>
-
-          <div className="carousel-item">
-            <img src="src\assets\img\imagen4g.jpg" className="d-block w-100" alt="..."/>
-          </div>
-
+import "../assets/css/Home.css";
+import carrusel1 from "../assets/img/1.png";
+import carrusel2 from "../assets/img/2.png";
+import somos from "../assets/img/somos.png";
+import testimonio1 from "../assets/img/testimonio1.png";
+import testimonio2 from "../assets/img/testimonio2.png";
+import testimonio3 from "../assets/img/testimonio3.png";
+function Home() {
+  return (
+    <>
+      <Dash></Dash>
+      <div>
+        <div>
+          <Carrusel></Carrusel>
         </div>
-        
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-
+        <div className="m-5">
+          <Somos></Somos>
+        </div>
+        <div className="m-5">
+          <Testimonios></Testimonios>
+        </div>
       </div>
-    )
+    </>
+  );
 }
 
-function Servicios(){
-    return (
-      <nav className="direc bg-body-tertiary">
-        <div className="container-fluid">
-        <div className="direc-brand" href="#">
-          <p className="custom-paragraph">
-          Serdeco, una filial clave de la Corporación Eléctrica Nacional (CORPOELEC), desempeña un rol fundamental en la gestión de la facturación y recaudación del servicio eléctrico en Venezuela. Con una trayectoria que se remonta a su fundación en 1986 y su incorporación al gobierno en 2007, Serdeco se ha consolidado como el brazo operativo que garantiza la eficiencia en los procesos de facturación para CORPOELEC, así como para los servicios municipales de aseo y relleno sanitario. Su cobertura abarca un espectro amplio, trabajando estrechamente con alcaldías y gobernaciones para asegurar que los ciudadanos reciban y gestionen sus pagos de manera efectiva. La misión de Serdeco es optimizar estos servicios, asegurando no solo la recolección de pagos, sino también el mantenimiento de la infraestructura crítica que soporta el suministro eléctrico y los servicios de saneamiento, contribuyendo así al bienestar y la calidad de vida de la población venezolana.
+function Carrusel() {
+  return (
+    <div
+      id="carouselExampleAutoplaying"
+      className="carousel slide carousel-fade"
+      data-bs-ride="carousel"
+      data-bs-interval="3000" // <-- Añade este atributo
+    >
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={carrusel1} className="d-block w-100" alt="..." />
+        </div>
+
+        <div className="carousel-item">
+          <img src={carrusel2} className="d-block w-100" alt="..." />
+        </div>
+
+        <div className="carousel-item">
+          <img
+            src="src\assets\img\imagen4g.jpg"
+            className="d-block w-100"
+            alt="..."
+          />
+        </div>
+      </div>
+
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+}
+
+function Somos() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-5">
+          <img
+            src={somos}
+            alt="Servicios"
+            className="img-fluid  top-50 start-50"
+          />
+        </div>
+        <div className="col-md-6">
+          <h2 className="text-success fw-bold">Serdeco</h2>
+          <p>Juntos por una ciudad limpia!</p>
+          <h2 className="text-success fw-bold">¿Quiénes somos?</h2>
+          <p>
+            Líderes en la gestión comercial, facturación y recaudación de
+            servicios de aseo urbano y relleno sanitario, comprometidos con la
+            sostenibilidad y la mejora continua de la calidad de vida en las
+            ciudades
           </p>
-
-          <img src="src\assets\img\mision.jpg" alt="Logo" width="500" height="300" className="d-inline-block align-text-top"/>
+          <h2 className="text-success fw-bold">¿Qué hacemos?</h2>
+          <ul style={{ listStyleType: "none" }}>
+            <li>
+              <i className="bi bi-file-earmark-text me-2 text-success"></i>
+              Facturación oportuna.
+            </li>
+            <li>
+              <i className="bi bi-shield-check me-2 text-success"></i>
+              Garantizamos accesibilidad, confianza y seguridad.
+            </li>
+            <li>
+              <i className="bi bi-lightbulb me-2 text-success"></i>
+              Brindamos soluciones innovadoras para satisfacer las necesidades
+              de nuestros usuarios.
+            </li>
+          </ul>
         </div>
+      </div>
+    </div>
+  );
+}
 
-        
+function Testimonios() {
+  return (
+    <div className="container">
+      <h2 className="text-center text-success fs-1 mt-5 mb-5">Testimonios</h2>
+      <div className="row">
+        <div className="col-md-4 ">
+          <div className="text-center">
+            <img src={testimonio1} className="img-fluid rounded-circle" />
+          </div>
+          <h1> Usuario Satisfecho</h1>
+          <p>"Excelente servicio, siempre puntuales y amables."</p>
         </div>
-      </nav>
-
-      
-    )
+        <div className="col-md-4">
+          <img src={testimonio2} className="img-fluid rounded-circle" />
+          <h1> Usuario Satisfecho</h1>
+          <p>"La mejor empresa de aseo que he conocido."</p>
+        </div>
+        <div className="col-md-4">
+          <img src={testimonio3} className="img-fluid rounded-circle" />
+          <h1> Usuario Satisfecho</h1>
+          <p>"Muy profesionales y comprometidos con el medio ambiente."</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
